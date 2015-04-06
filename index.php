4fc0f1d07bf8b692e4858b9c7a776b31a541b902
Christopher Morrobel <cm288@afsconnect2.njit.edu>
2015-04-06 15:09:17 -0400

    trying to echo csv file

--- index.php
+++ index.php
@@ -1,22 +1,36 @@
 <?php
 
-
 class Singleton
 {
-	public static function tInstance()
-        {
+	public static function instance()
+	{
 		static $inst = null;
 		if (null === $inst) 
 		{
 			$inst = new static();
 		}
-		
+															
 		return $inst;
+	}
+
+	public function __construct()
+	{
+		$file = fopen("testcsv.csv","r");
+		//$data = fgetcsv($file);
+		while(!feof($file))
+		{
+			 echo fgetcsv($file). "<br />";
+		}
 		
+		fclose($file);
 	}
 
 }
 
+$object = new Singleton();
 
+
+
+echo "this is a test";
 
 ?>
